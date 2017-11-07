c46925cf8e2df5d1e51b1ead4565c69a2810606c
Paul Hummer <paul@eventuallyanyway.com>
2017-11-06 22:05:19 -0700

    Handle up/down/left/right controls
    `event::EventHandler` is nice, but the actual callback function
    `key_down_event` doesn't provide the context. I think the intent is
    to force implementers to do what I did and control input state with
    a struct. In this case, I set `InputState` inputs and let the `update`
    function handle how to deal with those inputs, and then resetting them
    for next time.
    
    I also changed the `update` function to update the position of the main
    sprite based on how much time has passed since the last time update
    was called. This means that, should the event loop experience some
    lag due to badly structured code, the movement is still a consistent
    rate throughout.

--- src/main.rs
+++ src/main.rs
@@ -7,37 +7,101 @@ use ggez::graphics;
 use ggez::graphics::{Color, DrawMode, Point};
 use std::time::Duration;
 
+const SPEED: f32 = 250.0;
+const SIZE: f32 = 50.0;
+
+struct InputState {
+    up: bool,
+    down: bool,
+    left: bool,
+    right: bool,
+}
+
+impl InputState {
+    fn new() -> InputState {
+        return InputState {
+                   up: false,
+                   down: false,
+                   left: false,
+                   right: false,
+               };
+    }
+
+    fn reset(&mut self) {
+        self.up = false;
+        self.down = false;
+        self.left = false;
+        self.right = false;
+    }
+}
+
 struct MainState {
-    pos_x: f32,
+    x: f32,
+    y: f32,
+    input_state: InputState,
 }
 
 impl MainState {
     fn new(ctx: &mut Context) -> GameResult<MainState> {
-        let s = MainState { pos_x: 0.0 };
+        let window = ctx.gfx_context.get_window();
+        let (width, height) = window.size();
+        let s = MainState {
+            x: width as f32 * 0.5,
+            y: height as f32 * 0.5,
+            input_state: InputState::new(),
+        };
         Ok(s)
     }
 }
 
 impl event::EventHandler for MainState {
-    fn update(&mut self, _ctx: &mut Context, _dt: Duration) -> GameResult<()> {
-        self.pos_x = self.pos_x % 800.0 + 1.0;
+    fn update(&mut self, ctx: &mut Context, dt: Duration) -> GameResult<()> {
+        let float_duration = dt.as_secs() as f64 + dt.subsec_nanos() as f64 * 1e-9;
+
+        if self.input_state.up {
+            self.y = self.y - (SPEED * float_duration as f32);
+        }
+        if self.input_state.down {
+            self.y = self.y + (SPEED * float_duration as f32);
+        }
+        if self.input_state.left {
+            self.x = self.x - (SPEED * float_duration as f32);
+        }
+        if self.input_state.right {
+            self.x = self.x + (SPEED * float_duration as f32);
+        }
+        self.input_state.reset();
+
         Ok(())
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
         graphics::clear(ctx);
-        graphics::circle(
-            ctx,
-            DrawMode::Fill,
-            Point {
-                x: self.pos_x,
-                y: 380.0,
-            },
-            100.0,
-            32,
-        )?;
+        graphics::circle(ctx,
+                         DrawMode::Fill,
+                         Point {
+                             x: self.x,
+                             y: self.y,
+                         },
+                         SIZE,
+                         32)?;
         graphics::present(ctx);
         Ok(())
+    }
+
+    fn key_down_event(&mut self, keycode: event::Keycode, keymod: event::Mod, repeat: bool) {
+        match keycode {
+            event::Keycode::Up => self.input_state.up = true,
+            event::Keycode::Down => self.input_state.down = true,
+            event::Keycode::Left => self.input_state.left = true,
+            event::Keycode::Right => self.input_state.right = true,
+            _ => {
+                println!("Key pressed: {:?}, modifier {:?}, repeat: {}",
+                         keycode,
+                         keymod,
+                         repeat);
+            }
+        }
     }
 }
 

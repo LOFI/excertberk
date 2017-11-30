d7bb165caccea779e50b75788d8179f391d826ad
Owen Nelson <onelson@gmail.com>
2017-11-30 00:40:06 -0800

    Rework all sorts of tile positioning nonsense.
    ugh, x/y on rect is *center* of shape. Also 0,0 is bottom left with y
    pointing up. I had thought 0,0 was top left with y pointing down, but
    that's not the case at all.
    
    New plan for tile rendering involves updating to ggez master where the
    new SpriteBatch is available. We should be able to render the track as
    one big texture then just translate it. That should be much better than
    assembling tiles each tick.

--- eb_core/track.rs
+++ eb_core/track.rs
@@ -5,6 +5,8 @@ use serde_json;
 use serde_json::Value;
 use ggez::graphics::Rect;
 
+
+type GID = u32;
 pub struct Image;
 pub struct Object;
 
@@ -21,7 +23,7 @@ pub struct TileLayerData {
     pub name: String,
     pub height: u32,
     pub width: u32,
-    pub data: Vec<u32>,
+    pub data: Vec<GID>,
 }
 
 
@@ -33,7 +35,7 @@ pub struct ObjectLayerData {
 }
 
 
-fn div_mod(n: u32, div: u32) -> (u32, u32) {
+fn divmod(n: u32, div: u32) -> (u32, u32) {
     (n / div, n % div)
 }
 
@@ -63,32 +65,37 @@ impl Track {
 
 
 
-/// Converts a GID into a `Rect` region of a tileset image.
+/// Converts a tile index into a `Rect` region of a tileset image.
 ///
 /// The `tileset_width` and `tileset_height` params are the number of cells (not
 /// pixels) that make up the entire image.
 /// The image size is therefore:
 ///   `(tileset_width * tile_width) * (tileset_height * tile_height)`
 /// Panics when gid is out of bounds.
-fn gid_to_rect(
-    gid: u32,
+fn tile_idx_to_rect(
+    idx: u32,
     tile_width: u32,
     tile_height: u32,
     tileset_width: u32,
     tileset_height: u32,
 ) -> Rect {
-    let (row, col) = div_mod(gid, tileset_width);
+    let (row, col) = divmod(idx, tileset_width);
     assert!(
         row < tileset_height,
         "gid bounds check failed, row too large."
     );
+
+    let half_width = (tile_width as f32) / 2.;
+    let half_height = (tile_height as f32) / 2.;
+
     Rect::new(
-        (col * tile_width) as f32,
-        (row * tile_height) as f32,
+        ((col * tile_width) as f32) + half_width,
+        (((tileset_height - row - 1) * tile_height) as f32) + half_height,
         tile_width as f32,
         tile_height as f32,
     )
 }
+
 
 #[cfg(test)]
 mod tests {
@@ -96,74 +103,101 @@ mod tests {
 
     #[test]
     fn test_gid_0_to_rect() {
-        assert_eq!(gid_to_rect(0, 16, 16, 3, 3), Rect::new(0., 0., 16., 16.));
+        assert_eq!(
+            tile_idx_to_rect(0, 16, 16, 3, 3),
+            Rect::new_i32(8, 40, 16, 16)
+        );
     }
 
     #[test]
     fn test_gid_1_to_rect() {
-        assert_eq!(gid_to_rect(1, 16, 16, 3, 3), Rect::new(16., 0., 16., 16.));
+        assert_eq!(
+            tile_idx_to_rect(1, 16, 16, 3, 3),
+            Rect::new_i32(24, 40, 16, 16)
+        );
     }
 
     #[test]
     fn test_gid_2_to_rect() {
-        assert_eq!(gid_to_rect(2, 16, 16, 3, 3), Rect::new(32., 0., 16., 16.));
+        assert_eq!(
+            tile_idx_to_rect(2, 16, 16, 3, 3),
+            Rect::new_i32(40, 40, 16, 16)
+        );
     }
 
     #[test]
     fn test_gid_3_to_rect() {
-        assert_eq!(gid_to_rect(3, 16, 16, 3, 3), Rect::new(0., 16., 16., 16.));
+        assert_eq!(
+            tile_idx_to_rect(3, 16, 16, 3, 3),
+            Rect::new_i32(8, 24, 16, 16)
+        );
     }
 
     #[test]
     fn test_gid_4_to_rect() {
-        assert_eq!(gid_to_rect(4, 16, 16, 3, 3), Rect::new(16., 16., 16., 16.));
+        assert_eq!(
+            tile_idx_to_rect(4, 16, 16, 3, 3),
+            Rect::new_i32(24, 24, 16, 16)
+        );
     }
 
     #[test]
     fn test_gid_5_to_rect() {
-        assert_eq!(gid_to_rect(5, 16, 16, 3, 3), Rect::new(32., 16., 16., 16.));
+        assert_eq!(
+            tile_idx_to_rect(5, 16, 16, 3, 3),
+            Rect::new_i32(40, 24, 16, 16)
+        );
     }
 
     #[test]
     fn test_gid_6_to_rect() {
-        assert_eq!(gid_to_rect(6, 16, 16, 3, 3), Rect::new(0., 32., 16., 16.));
+        assert_eq!(
+            tile_idx_to_rect(6, 16, 16, 3, 3),
+            Rect::new_i32(8, 8, 16, 16)
+        );
     }
 
     #[test]
     fn test_gid_7_to_rect() {
-        assert_eq!(gid_to_rect(7, 16, 16, 3, 3), Rect::new(16., 32., 16., 16.));
+        assert_eq!(
+            tile_idx_to_rect(7, 16, 16, 3, 3),
+            Rect::new_i32(24, 8, 16, 16)
+        );
     }
 
     #[test]
     fn test_gid_8_to_rect() {
-        assert_eq!(gid_to_rect(8, 16, 16, 3, 3), Rect::new(32., 32., 16., 16.));
+        assert_eq!(
+            tile_idx_to_rect(8, 16, 16, 3, 3),
+            Rect::new_i32(40, 8, 16, 16)
+        );
     }
 
     #[test]
     #[should_panic]
     fn test_out_of_bounds_gid() {
-        gid_to_rect(9, 16, 16, 3, 3);
+        tile_idx_to_rect(9, 16, 16, 3, 3);
     }
 
     // I probably don't need these divmod tests, but it is very late...
     #[test]
-    fn test_div_mod_3_2() {
-        assert_eq!(div_mod(3, 2), (1, 1));
+    fn test_divmod_3_2() {
+        assert_eq!(divmod(3, 2), (1, 1));
     }
 
     #[test]
-    fn test_div_mod_5_5() {
-        assert_eq!(div_mod(5, 5), (1, 0));
+    fn test_divmod_5_5() {
+        assert_eq!(divmod(5, 5), (1, 0));
     }
 
     #[test]
-    fn test_div_mod_5_10() {
-        assert_eq!(div_mod(5, 10), (0, 5));
+    fn test_divmod_5_10() {
+        assert_eq!(divmod(5, 10), (0, 5));
     }
 
     #[test]
-    fn test_div_mod_5_2() {
-        assert_eq!(div_mod(5, 2), (2, 1));
+    fn test_divmod_5_2() {
+        assert_eq!(divmod(5, 2), (2, 1));
     }
 
 }
